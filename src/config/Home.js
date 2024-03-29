29c688c3552c35aad2b0cb2996fcb9efb3440970
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-28 20:37:46 -0400

    chore (setup) data table

--- src/config/Home.js
+++ src/config/Home.js
@@ -1,14 +1,24 @@
 import supabase from "../config/supabaseClient";
+import { usaeEffect, useEffect, useState } from "react";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [bubbles, setBubbles] = useState(null);
+
+  useEffect(() => {
+    const fetchBubbles = async () => {
+      const { data, error } = await supabase.from("bubbles").select();
+
+      if (error) {
+        setFetchError("Could not fetch the bubbles");
+        console.log(error);
+      }
+    };
+  }, []);
 
   return (
     <div className="page home">
       <h2 className="text-3xl font-bold underline">Home</h2>
-      <form type="input" placeholder="Bubbles">
-        <input>Bubbles</input>
-      </form>
     </div>
   );
 };
